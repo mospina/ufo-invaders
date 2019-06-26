a476e308c0ece86eb08c22e7a4018ccce58be23b
Manuel Ospina <manuel@anect.jp>
2019-06-26 13:25:09 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,25 @@
+# Elm
+
+## Start project
+
+elm init
+
+## Set up testing
+
+npm install -g elm-test
+elm-test init
+
+## Development environment
+
+elm reactor
+
+## Installing 3th party libs
+
+elm install timjs/elm-collage
+
+
+## Images and assets
+
+https://opengameart.org/
+starfighter: https://opengameart.org/content/starfighter-x-3
+Nave: https://opengameart.org/content/alien-spaceship
---
+++ src/Main.elm
@@ -0,0 +1,73 @@
+module Main exposing (Model, Msg(..), init, main, subscriptions, update, view)
+
+import Browser
+import Html exposing (..)
+
+
+
+-- MAIN
+
+
+main =
+    Browser.element
+        { init = init
+        , view = view
+        , update = update
+        , subscriptions = subscriptions
+        }
+
+
+
+-- MODEL
+
+
+type alias Model =
+    {}
+
+
+init : () -> ( Model, Cmd Msg )
+init _ =
+    ( {}
+    , Cmd.none
+    )
+
+
+
+-- UPDATE
+
+
+type Msg
+    = Left
+    | Right
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        Left ->
+            ( {}
+            , Cmd.none
+            )
+
+        Right ->
+            ( {}
+            , Cmd.none
+            )
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+    Sub.none
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    h1 [] [ text "Output goes here" ]
