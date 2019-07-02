73dee62459d4ce589924efe73fd1c7ea0318874c
Manuel Ospina <manuel@anect.jp>
2019-07-02 10:38:00 +0900

    Implement View

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,7 @@
-module Main exposing (Game, Invader, Missile, Model, Msg(..), State(..), Tank, assetsDir, gameHeight, gameWidth, hitRange, init, invaderImage, invaderRate, invaderXspeed, invaderYspeed, main, missileImage, missileSpeed, subscriptions, tankImage, tankSpeed, tankY, update, view)
+module Main exposing (Game, Invader, Missile, Model, Msg(..), State(..), Tank, assetsDir, gameHeight, gameWidth, halfGameHeight, halfGameWidth, hitRange, init, invaderHeight, invaderImage, invaderRate, invaderWidth, invaderXspeed, invaderYspeed, main, missileHeight, missileImage, missileSpeed, missileWidth, render, renderInvader, renderListOfInvaders, renderListOfMissiles, renderMissile, renderTank, subscriptions, tankHeight, tankImage, tankSpeed, tankWidth, tankY, update, view)
 
 import Browser
-import Collage exposing (image)
+import Collage exposing (Collage, group, image, shift)
 import Collage.Render exposing (svgBox)
 import Html exposing (..)
 
@@ -33,12 +33,28 @@ gameHeight =
     600
 
 
+halfGameWidth =
+    gameWidth / 2
+
+
+halfGameHeight =
+    gameHeight / 2
+
+
 assetsDir =
     "../assets/"
 
 
+tankWidth =
+    20
+
+
+tankHeight =
+    20
+
+
 tankY =
-    gameHeight - 50
+    0 - (halfGameHeight - (tankHeight / 2))
 
 
 tankImage =
@@ -49,6 +65,14 @@ tankSpeed =
     2
 
 
+missileHeight =
+    15
+
+
+missileWidth =
+    5
+
+
 missileImage =
     assetsDir ++ "Missile.png"
 
@@ -57,11 +81,20 @@ missileSpeed =
     5
 
 
+invaderHeight =
+    15
+
+
+invaderWidth =
+    20
+
+
 invaderImage =
     assetsDir ++ "Nave.png"
 
 
 invaderXspeed =
+    -- I may don't need this one
     1.5
 
 
@@ -82,12 +115,12 @@ hitRange =
 
 
 type alias Tank =
-    { x : Int }
+    { x : Float }
 
 
 initialTank : Tank
 initialTank =
-    Tank (gameWidth // 2)
+    Tank 0
 
 
 
@@ -110,26 +143,29 @@ initialTank =
 
 
 type alias Invader =
-    { x : Int
-    , y : Int
+    { x : Float
+    , y : Float
+    , speedX : Float
     }
 
 
 
 {-
    -- Invader is: Invader x y
-   -- interp. a invader at position x y
+   -- interp. a invader at position x y,
+              and speedX per clock tick along x axis (negative number left, positive number right)
             NOTE: the starter file declares dx: the invader along x by dx pixels per clock tick
 
-   invader  = Invader 6 16
+   invader  = Invader 6 16 -3
 
    fnForInvader : Invader -> ...
    fnForInvader invader =
-     ... invader.x  -- number
-         invader.y  -- number
+     ... invader.x   -- number
+         invader.y   -- number
+         invader.speedX -- number [-1,1]
 
    -- Template rules used:
-   --   - compound: 2 fields
+   --   - compound: 3 fields
 -}
 {-
    -- [Invader] is one of:
@@ -137,13 +173,13 @@ type alias Invader =
    --   - Invader :: [Invader]
    -- interp. a list of Invader
    invader0 = []
-   invader1 = [Invader 10 20, Invader 20 20]
+   invader1 = [Invader 10 20 3, Invader 20 20 -3]
 
    fnForListOfInvaders : [Invader] -> ...
    fnForListOfInvaders invaders =
      case invaders of
        [] -> []
-       (first:rest) -> (fnForInvader first) :: (fnForListOfInvaders rest)
+       first::rest -> (fnForInvader first) :: (fnForListOfInvaders rest)
 
    -- Template rules used:
    --   - one of: 2 cases
@@ -155,8 +191,8 @@ type alias Invader =
 
 
 type alias Missile =
-    { x : Int
-    , y : Int
+    { x : Float
+    , y : Float
     }
 
 
@@ -332,8 +368,53 @@ subscriptions model =
 
 
 -- VIEW
+-- Render current state of the game in the screen according to the model
 
 
 view : Model -> Html Msg
 view model =
-    h1 [] [ text "Output goes here" ]
+    svgBox ( gameHeight, gameWidth ) <| render model.game
+
+
+render : Game -> Collage msg
+render game =
+    let
+        gameObjects =
+            renderTank game.tank :: renderListOfInvaders game.invaders ++ renderListOfMissiles game.missiles
+    in
+    group gameObjects
+
+
+renderTank : Tank -> Collage msg
+renderTank tank =
+    image ( tankHeight, tankWidth ) tankImage |> shift ( tank.x, tankY )
+
+
+renderListOfInvaders : List Invader -> List (Collage msg)
+renderListOfInvaders invaders =
+    case invaders of
+        [] ->
+            []
+
+        first :: rest ->
+            renderInvader first :: renderListOfInvaders rest
+
+
+renderListOfMissiles : List Missile -> List (Collage msg)
+renderListOfMissiles missiles =
+    case missiles of
+        [] ->
+            []
+
+        first :: rest ->
+            renderMissile first :: renderListOfMissiles rest
+
+
+renderInvader : Invader -> Collage msg
+renderInvader invader =
+    image ( invaderHeight, invaderWidth ) invaderImage |> shift ( invader.x, invader.y )
+
+
+renderMissile : Missile -> Collage msg
+renderMissile missile =
+    image ( missileHeight, missileWidth ) missileImage |> shift ( missile.x, missile.y )
--- tests/Example.elm
+++ tests/Example.elm
@@ -1,5 +1,7 @@
-module Example exposing (suite)
+module Example exposing (testInit, testRenderFunctions)
 
+import Collage exposing (group, image, shift)
+import Collage.Render exposing (svgBox)
 import Expect exposing (Expectation)
 import Fuzz exposing (Fuzzer, int, list, string)
 import Main exposing (..)
@@ -11,27 +13,27 @@ import Test exposing (..)
 
 
 tankCenter =
-    Tank (gameWidth // 2)
-
-
-tankLeft =
     Tank 0
 
 
+tankLeft =
+    Tank (0 - (gameWidth / 2))
+
+
 tankRigth =
-    Tank gameWidth
+    Tank (gameWidth / 2)
 
 
 invader1 =
-    Invader 10 10
+    Invader 0 0 3
 
 
 invader2 =
-    Invader (gameHeight // 2) (gameWidth // 2)
+    Invader (halfGameHeight / 2) (halfGameWidth / 2) -3
 
 
 invader3 =
-    Invader (gameHeight - 30) (gameWidth - 20)
+    Invader (0 - halfGameHeight) (0 - halfGameWidth) 3
 
 
 emptyListOfInvaders =
@@ -43,15 +45,15 @@ listOfInvaders =
 
 
 missile1 =
-    Missile (gameHeight - 20) 10
+    Missile -20 10
 
 
 missile2 =
-    Missile (gameHeight // 2) (gameWidth // 2)
+    Missile 0 0
 
 
 missile3 =
-    Missile 10 (gameWidth - 20)
+    Missile 10 -20
 
 
 emptyListOfMissiles =
@@ -82,8 +84,8 @@ endModel =
     Model GameOver initialGame
 
 
-suite : Test
-suite =
+testInit : Test
+testInit =
     describe "init"
         [ test "return initial Model" <|
             \_ ->
@@ -92,4 +94,142 @@ suite =
                         init ()
                 in
                 model |> Expect.equal initialModel
+        ]
+
+
+testRenderFunctions : Test
+testRenderFunctions =
+    describe "Render functions"
+        [ describe "view"
+            [ test "return the initial view" <|
+                \_ ->
+                    let
+                        renderedView =
+                            view initialModel
+
+                        initialView =
+                            svgBox ( gameHeight, gameWidth ) <|
+                                group [ image ( tankHeight, tankWidth ) tankImage |> shift ( tankCenter.x, tankY ) ]
+                    in
+                    renderedView |> Expect.equal initialView
+            , test "return a play  view" <|
+                \_ ->
+                    let
+                        renderedView =
+                            view updatedModel
+
+                        updatedView =
+                            svgBox ( gameHeight, gameWidth ) <|
+                                group
+                                    [ image ( tankHeight, tankWidth ) tankImage |> shift ( tankLeft.x, tankY )
+                                    , image ( invaderHeight, invaderWidth ) invaderImage
+                                        |> shift ( invader1.x, invader1.y )
+                                    , image ( invaderHeight, invaderWidth ) invaderImage
+                                        |> shift ( invader2.x, invader2.y )
+                                    , image ( invaderHeight, invaderWidth ) invaderImage
+                                        |> shift ( invader3.x, invader3.y )
+                                    , image ( missileHeight, missileWidth ) missileImage
+                                        |> shift ( missile1.x, missile1.y )
+                                    , image ( missileHeight, missileWidth ) missileImage
+                                        |> shift ( missile2.x, missile2.y )
+                                    , image ( missileHeight, missileWidth ) missileImage
+                                        |> shift ( missile3.x, missile3.y )
+                                    ]
+                    in
+                    renderedView |> Expect.equal updatedView
+            ]
+        , describe "render"
+            [ test "create a collage with the game" <|
+                \_ ->
+                    let
+                        returnedGameCollage =
+                            render fullGame
+
+                        fullGameCollage =
+                            group
+                                [ image ( tankHeight, tankWidth ) tankImage |> shift ( tankLeft.x, tankY )
+                                , image ( invaderHeight, invaderWidth ) invaderImage
+                                    |> shift ( invader1.x, invader1.y )
+                                , image ( invaderHeight, invaderWidth ) invaderImage
+                                    |> shift ( invader2.x, invader2.y )
+                                , image ( invaderHeight, invaderWidth ) invaderImage
+                                    |> shift ( invader3.x, invader3.y )
+                                , image ( missileHeight, missileWidth ) missileImage
+                                    |> shift ( missile1.x, missile1.y )
+                                , image ( missileHeight, missileWidth ) missileImage
+                                    |> shift ( missile2.x, missile2.y )
+                                , image ( missileHeight, missileWidth ) missileImage
+                                    |> shift ( missile3.x, missile3.y )
+                                ]
+                    in
+                    returnedGameCollage |> Expect.equal fullGameCollage
+            ]
+        , describe "renderTank"
+            [ test "return a Collage of a tank" <|
+                \_ ->
+                    let
+                        returnedTankCollage =
+                            renderTank tankLeft
+
+                        tankLeftCollage =
+                            image ( tankHeight, tankWidth ) tankImage |> shift ( tankLeft.x, tankY )
+                    in
+                    returnedTankCollage |> Expect.equal tankLeftCollage
+            ]
+        , describe "render Lists"
+            [ test "render list of invaders" <|
+                \_ ->
+                    let
+                        returnedCollageList =
+                            renderListOfInvaders listOfInvaders
+
+                        invadersCollageList =
+                            [ image ( invaderHeight, invaderWidth ) invaderImage
+                                |> shift ( invader1.x, invader1.y )
+                            , image ( invaderHeight, invaderWidth ) invaderImage
+                                |> shift ( invader2.x, invader2.y )
+                            , image ( invaderHeight, invaderWidth ) invaderImage
+                                |> shift ( invader3.x, invader3.y )
+                            ]
+                    in
+                    returnedCollageList |> Expect.equal invadersCollageList
+            , test "render lists of missiles" <|
+                \_ ->
+                    let
+                        returnedCollageList =
+                            renderListOfMissiles listOfMissiles
+
+                        missilesCollageList =
+                            [ image ( missileHeight, missileWidth ) missileImage
+                                |> shift ( missile1.x, missile1.y )
+                            , image ( missileHeight, missileWidth ) missileImage
+                                |> shift ( missile2.x, missile2.y )
+                            , image ( missileHeight, missileWidth ) missileImage
+                                |> shift ( missile3.x, missile3.y )
+                            ]
+                    in
+                    returnedCollageList |> Expect.equal missilesCollageList
+            , test "render invader" <|
+                \_ ->
+                    let
+                        returnedCollage =
+                            renderInvader invader1
+
+                        invaderCollage =
+                            image ( invaderHeight, invaderWidth ) invaderImage
+                                |> shift ( invader1.x, invader1.y )
+                    in
+                    returnedCollage |> Expect.equal invaderCollage
+            , test "render missile" <|
+                \_ ->
+                    let
+                        returnedCollage =
+                            renderMissile missile1
+
+                        missileCollage =
+                            image ( missileHeight, missileWidth ) missileImage
+                                |> shift ( missile1.x, missile1.y )
+                    in
+                    returnedCollage |> Expect.equal missileCollage
+            ]
         ]
